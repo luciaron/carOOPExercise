fc5abd0ec405a379ceee4cbb7493210968a30752
aaron <aaronmlucia@gmail.com>
2021-12-13 21:38:00 -0500

    exercise complete

---
+++ oop-car-ex.js
@@ -0,0 +1,54 @@
+//part one
+class Vehicle {
+  constructor (make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  };
+
+  honk () {
+    return "Beep.";
+  };
+
+  toString () {
+    return `The vehicle is a ${this.make} ${this.model} from ${this.year}`;
+  };
+}
+
+//part two
+class Car extends Vehicle {
+  constructor (make, model, year) {
+    super(make, model, year);
+    this.numWheels = 4;
+  };
+}
+
+//part three
+class Motorcycle extends Vehicle {
+  constructor (make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+  };
+  revEngine () {
+    return "VROOM!!!";
+  };
+}
+
+//part four
+class Garage {
+  constructor (capacity) {
+    this.capacity = capacity;
+    this.vehicles = [];
+  };
+
+  add(newVehicle) {
+    if (!(newVehicle instanceof Vehicle)) {
+      return "Only vehicles are allowed in here!";
+    };
+    if (this.vehicles.length >= this.capacity) {
+      return "Sorry, we're full!";
+    }
+    this.vehicles.push(newVehicle);
+    return "Vehicle added!";
+  };
+}
